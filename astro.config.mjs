08377d8999bde137786bd59047bba2f84299b985
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-04-24 13:58:54 -0400

    install preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
+import preact from '@astrojs/preact';
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), preact()],
 });
