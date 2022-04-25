b16f26a854c3e9a5c7e2de631e9550359623e1f7
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-04-25 13:31:57 -0400

    remove turbolinks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
-import turbolinks from '@astrojs/turbolinks';
-
 export default defineConfig({
-  integrations: [tailwind(), preact(), turbolinks()],
+  integrations: [tailwind(), preact()],
 });
