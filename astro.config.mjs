24f0099973332789be5514eab20b9c427670e012
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-04-24 21:50:35 -0400

    add turbolinks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
+import turbolinks from '@astrojs/turbolinks';
+
 export default defineConfig({
-  integrations: [tailwind(), preact()],
+  integrations: [tailwind(), preact(), turbolinks()],
 });
