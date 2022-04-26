27a01608f7cac81184eb76d44af798287d254f3d
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-04-26 17:21:41 -0400

    set up astro image tools

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
+import astroImagePlugin from "astro-imagetools/plugin";
+
 export default defineConfig({
   integrations: [tailwind(), preact()],
+  vite: {
+    plugins: [astroImagePlugin,]
+  }
 });
