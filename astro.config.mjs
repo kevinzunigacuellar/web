15227801fff4b108a9e4f0e4fbef054174da66e1
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-05-01 22:39:44 -0400

    add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import preact from '@astrojs/preact'
 import { astroImageTools } from 'astro-imagetools'
+import sitemap from '@astrojs/sitemap'
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [astroImageTools, tailwind(), preact()],
+  integrations: [astroImageTools, tailwind(), preact(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: 'one-dark-pro',
