20c94cf5c2a1053a5c1bf526cf705af46967db32
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-06-08 23:03:00 -0400

    remove partytown integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,22 +2,11 @@ import { defineConfig } from 'astro/config'
 import { astroImageTools } from 'astro-imagetools'
 import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
-import partytown from '@astrojs/partytown'
 import solid from '@astrojs/solid-js'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    astroImageTools,
-    tailwind(),
-    solid(),
-    sitemap(),
-    partytown({
-      config: {
-        forward: ['dataLayer.push'],
-      },
-    }),
-  ],
+  integrations: [astroImageTools, tailwind(), solid(), sitemap()],
   site: 'https://kevinzunigacuellar.com',
   markdown: {
     shikiConfig: {
