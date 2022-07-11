fb7a32fdcfa186c1f6bf910e66a712f0a8041b89
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-07-11 16:49:24 -0400

    integrate image tool to index and bookshelf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,17 @@
 import { defineConfig } from 'astro/config'
-import { astroImageTools } from 'astro-imagetools'
 import tailwind from '@astrojs/tailwind'
+import image from '@astrojs/image'
 import sitemap from '@astrojs/sitemap'
 import solid from '@astrojs/solid-js'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [astroImageTools, tailwind(), solid(), sitemap()],
+  integrations: [tailwind(), solid(), sitemap(), image()],
   site: 'https://kevinzunigacuellar.com',
   markdown: {
     shikiConfig: {
       theme: 'one-dark-pro',
       wrap: true,
     },
-  },
-  vite: {
-    ssr: {
-      external: ['svgo'],
-    },
-  },
-  experimental: {
-    integrations: true,
   },
 })
