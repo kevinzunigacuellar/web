5cc6d6483ed917680b7e08c4261a3154968ad3cb
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-07-11 20:33:51 -0400

    add prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import image from '@astrojs/image'
 import sitemap from '@astrojs/sitemap'
+import prefetch from '@astrojs/prefetch';
 import solid from '@astrojs/solid-js'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solid(), sitemap(), image()],
+  integrations: [tailwind(), solid(), sitemap(), image(), prefetch()],
   site: 'https://kevinzunigacuellar.com',
   markdown: {
     shikiConfig: {
