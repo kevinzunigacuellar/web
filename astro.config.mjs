33b4ae8f6a999a9233f7d49972801b73a75c19dc
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-07-27 17:59:46 -0400

    remove prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
-import prefetch from "@astrojs/prefetch";
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), image(), prefetch(), mdx()],
-  site: "https://kevinzunigacuellar.com",
+  integrations: [tailwind(), sitemap(), image(), mdx()],
+  site: "https://www.kevinzunigacuellar.com",
   markdown: {
     shikiConfig: {
       theme: "one-dark-pro",
