d425423931417f4f0ab5b371be8c5fd07198e3ec
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-07-27 17:49:43 -0400

    add mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), image(), prefetch()],
+  integrations: [tailwind(), sitemap(), image(), prefetch(), mdx()],
   site: "https://kevinzunigacuellar.com",
   markdown: {
     shikiConfig: {
