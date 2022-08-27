549decd5bbcff03592c9a7539eb8bc743372a1ff
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-08-26 23:09:39 -0400

    adds fancy inline code

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,28 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,astro}"],
   theme: {
     extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            'code::before': {
+              content: '""'
+            },
+            'code::after': {
+              content: '""'
+            },
+            p: {
+              code:{
+                backgroundColor: '#282c34',
+                padding: '0.250rem',
+                borderRadius: '0.250rem',
+                fontWeight: '400',
+              }
+            }
+          }
+        }
+      },
       keyframes: {
         "move-bg": {
           to: {
