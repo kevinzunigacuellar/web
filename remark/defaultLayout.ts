95fee3d4bb89a1242c4d79d3ee8b464355b26632
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-10-02 21:06:20 -0400

    fix eslint

--- remark/defaultLayout.ts
+++ remark/defaultLayout.ts
@@ -1,7 +1,7 @@
 // sets a default layout for all mdx files
 
 export default function setFallbackLayout() {
-  return function (_tree : any, file: any) {
+  return function (_tree, file) {
     const layout = file.data.astro.frontmatter.layout ?? "@layouts/BlogLayout.astro";
     file.data.astro.frontmatter.layout = layout;
   };
