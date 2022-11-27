68237195dd9346638f372823a0dc00465fe7075a
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-11-27 02:47:39 -0500

    setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,39 +1,24 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import rehypePrettyCode from "rehype-pretty-code";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import astroLayouts from "astro-layouts";
+import codeTitle from "remark-code-title";
 
 const layoutOptions = {
   "pages/blog/**/*": "@layouts/BlogLayout.astro",
 };
 
-const prettyCodeOptions = {
-  theme: "one-dark-pro",
-  onVisitHighlightedLine(node) {
-    node.properties.className.push("highlighted");
-  },
-  onVisitHighlightedWord(node) {
-    node.properties.className = ["word"];
-  },
-};
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    sitemap(),
-    image(),
-    mdx({
-      remarkPlugins: [[astroLayouts, layoutOptions]],
-      rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
-      extendPlugins: "astroDefaults",
-    }),
-  ],
+  integrations: [tailwind(), sitemap(), image(), mdx()],
   site: "https://www.kevinzunigacuellar.com",
   markdown: {
-    syntaxHighlight: false,
+    extendDefaultPlugins: true,
+    remarkPlugins: [[astroLayouts, layoutOptions], codeTitle],
+    shikiConfig: {
+      theme: "dark-plus",
+    },
   },
 });
