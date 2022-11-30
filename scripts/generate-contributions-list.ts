a44324064f9ef75c7b379aed04fa3a145dd618b7
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-11-29 19:57:57 -0500

    add fetch to personal projects

--- scripts/generate-contributions-list.ts
+++ scripts/generate-contributions-list.ts
@@ -1,12 +1,10 @@
-import fs from 'node:fs/promises';
-import fetch from "node-fetch";
+import fs from "node:fs/promises";
+import { Octokit } from "@octokit/core";
 
 interface DataType {
-  data: {
-    viewer: {
-      repositoriesContributedTo: {
-        nodes: Repo[];
-      };
+  viewer: {
+    repositoriesContributedTo: {
+      nodes: Repo[];
     };
   };
 }
@@ -14,6 +12,7 @@ interface DataType {
 interface Repo {
   nameWithOwner: string;
   stargazerCount: number;
+  description: string;
   url: string;
   languages: {
     nodes: Language[];
@@ -24,57 +23,74 @@ interface Language {
   name: string;
 }
 
-async function getOpenSourceContributions() {
-  const res = await fetch("https://api.github.com/graphql", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
-    },
-    body: JSON.stringify({
-      query: `
-      query {
-        viewer {
-          repositoriesContributedTo(
-            first: 20
-            orderBy: {field: STARGAZERS, direction: DESC}
-            contributionTypes: [COMMIT, ISSUE, PULL_REQUEST, REPOSITORY]
-          ) {
-            nodes {
-              nameWithOwner
-              stargazerCount
-              url
-              languages(first: 4, orderBy: {field: SIZE, direction: DESC}) {
-                nodes {
-                  name
-                }
-              }
-            }
-          }
-        }
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+});
+
+const response = (await octokit.graphql(
+  `query {
+    viewer {
+      repositoriesContributedTo(
+      first: 20
+      orderBy: {field: STARGAZERS, direction: DESC}
+      contributionTypes: [COMMIT, ISSUE, PULL_REQUEST, REPOSITORY]
+    ) {
+        nodes {
+        nameWithOwner
+        description
+        stargazerCount
+        url
+        languages(first: 4, orderBy: {field: SIZE, direction: DESC}) {
+          nodes {
+          name
+        }}
       }
-    `,
-    }),
+    }
+  }
+}`,
+  { login: "kevinzunigacuellar" }
+)) as DataType;
+
+const maintainerRepos = new Set(["withastro/docs"]);
+const projectList = new Set(["remark-code-title", "astro-layouts"]);
+
+let authoredProjects = await Promise.all(
+  [...projectList].map(async (repo) => {
+    const { data } = await octokit.request("GET /repos/{owner}/{repo}", {
+      owner: "kevinzunigacuellar",
+      repo,
+    });
+    return {
+      name: data.name,
+      stars: data.stargazers_count,
+      url: data.html_url,
+      description: data.description,
+      languages: [data.language],
+      role: "author",
+    };
+  })
+);
+
+const contributions = response.viewer.repositoriesContributedTo.nodes
+  .filter((repo) => repo.languages.nodes.length > 1)
+  .map((repository) => {
+    return {
+      name: repository.nameWithOwner,
+      stars: repository.stargazerCount,
+      url: repository.url,
+      description: repository.description,
+      languages: repository.languages.nodes.map((language) => language.name),
+      role: maintainerRepos.has(repository.nameWithOwner)
+        ? "maintainer"
+        : "contributor",
+    };
   });
 
-  const { data } = await res.json() as DataType;
-  const contributions = data.viewer.repositoriesContributedTo.nodes
-    .filter((repo) => repo.languages.nodes.length > 1)
-    .map((repository) => {
-      return {
-        name: repository.nameWithOwner,
-        stars: repository.stargazerCount,
-        url: repository.url,
-        languages: repository.languages.nodes.map((language) => language.name),
-      };
-    });
-  return contributions;
-}
+const all = [...authoredProjects,...contributions,];
 
-const contributions = await getOpenSourceContributions();
 await fs.writeFile(
   "./src/content/contributions.json",
-  JSON.stringify(contributions, null, 2)
+  JSON.stringify(all, null, 2)
 );
 
 console.log("Contributions list generated");
