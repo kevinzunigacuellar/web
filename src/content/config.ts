d70586c8a5277bdc14195549bac3721cbd201089
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2023-02-12 23:15:30 -0500

    update schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,10 +2,12 @@ import { defineCollection, z } from "astro:content";
 
 const blogCollection = defineCollection({
   schema: z.object({
-    title: z.string(),
-    heroImage: z.string(),
-    imageAlt: z.string(),
-    description: z.string(),
+    title: z.string().max(60).min(10),
+    image: z.object({
+      src: z.string(),
+      alt: z.string(),
+    }),
+    description: z.string().max(160),
     pubDate: z.date(),
   }),
 });
