2cc61dffb33f662ba70db3407f83720cbf86fc7c
Kevin <kzunigac@uvm.edu>
2023-02-19 00:12:08 -0500

    remove unused deps

--- astro.config.ts
+++ astro.config.ts
@@ -3,20 +3,15 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
-import astroLayouts from "astro-layouts";
 import codeTitle from "remark-code-title";
 import fs from "node:fs";
-
-const layoutOptions = {
-  "pages/blog/**/*": "@layouts/BlogLayout.astro",
-};
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap(), image(), mdx()],
   site: "https://www.kevinzunigacuellar.com",
   markdown: {
-    remarkPlugins: [[astroLayouts, layoutOptions], codeTitle],
+    remarkPlugins: [codeTitle],
     shikiConfig: {
       theme: "dark-plus",
     },
