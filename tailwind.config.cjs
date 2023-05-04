ef7842ed118086564a0b4189480abb35eea5cbbd
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2023-05-04 17:59:55 -0400

    set mode to media

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,astro}"],
+  darkMode: "media",
   theme: {
     extend: {
       fontFamily: {
