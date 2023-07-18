5e4f88170a4909a7078514500861ed87e8adb017
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2023-07-17 21:50:36 -0400

    add expressive code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,34 @@
 import { defineConfig, sharpImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import codeTitle from "remark-code-title";
 import { readFileSync } from "node:fs";
+import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), expressiveCode()],
   experimental: {
-    assets: true,
+    assets: true
   },
   image: {
-    service: sharpImageService(),
+    service: sharpImageService()
   },
   compressHTML: true,
   build: {
-    inlineStylesheets: "auto",
+    inlineStylesheets: "auto"
   },
   site: "https://www.kevinzunigacuellar.com",
   markdown: {
-    remarkPlugins: [codeTitle],
     shikiConfig: {
-      theme: "dark-plus",
-    },
+      theme: "dark-plus"
+    }
   },
   vite: {
     plugins: [rawFonts([".ttf", ".woff"])],
     optimizeDeps: {
-      exclude: ["@resvg/resvg-js"],
-    },
-  },
+      exclude: ["@resvg/resvg-js"]
+    }
+  }
 });
 
 // vite plugin to import fonts
@@ -37,13 +36,13 @@ function rawFonts(ext) {
   return {
     name: "vite-plugin-raw-fonts",
     transform(_, id) {
-      if (ext.some((e) => id.endsWith(e))) {
+      if (ext.some(e => id.endsWith(e))) {
         const buffer = readFileSync(id);
         return {
           code: `export default ${JSON.stringify(buffer)}`,
-          map: null,
+          map: null
         };
       }
-    },
+    }
   };
 }
